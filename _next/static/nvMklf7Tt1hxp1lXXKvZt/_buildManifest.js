849d8451f42838a3839744c5609061f742b179b9
aliasjupleks <>
2021-11-23 03:12:29 +0000

    Deploying to gh-pages from @ aliasjupleks/kulyah@3f9a88fce06b6b233e6c0aab4aae57a77e1b127b 🚀

---
+++ _next/static/nvMklf7Tt1hxp1lXXKvZt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/732-8ebf0e0a74466f5f.js","static/chunks/pages/index-ebac0cd9263ee40b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
